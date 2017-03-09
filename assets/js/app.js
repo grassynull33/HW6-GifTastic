3ff25a2ec56a3e55f59186f6aadb4de41fbb7149
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-03-08 19:44:55 -0800

    add input for babe and generate button based on data validation (can't be blank input value or already in the array of babes)

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,19 @@ for(var i = 0; i < topics.length; i++) {
 	button.attr("data-babe", topics[i]);
 	$("#button-group").append(button);
 }
+
+$("#add-babe-button").on("click", function(e) {
+	e.preventDefault();
+	var alreadyExist = false;
+	if(topics.indexOf($("#new-babe-input").val()) !== -1) {
+		alreadyExist = true;
+	}
+	if($("#new-babe-input").val() !== "" && alreadyExist === false) {
+		var newBabe = $("#new-babe-input").val().toLowerCase();
+		topics.push(newBabe);
+		var button = $("<button>").text(newBabe);
+		button.attr("data-babe", newBabe);
+		$("#button-group").append(button);
+	}
+	$("#new-babe-input").val("");
+});
